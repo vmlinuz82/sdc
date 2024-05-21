7a11003a2ad376b22a49554587de5097a368f876
Konstantin Ivanov <konstantin.i@siteground.com>
2024-05-21 15:05:47 +0300

    Day1

---
+++ app/src/Entity/DragonTreasure.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
+#[ApiResource(
+    shortName: 'Treasure',
+    description: 'A rare and valuable treasure',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+    ],
+)]
+class DragonTreasure
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $value = null;
+
+    #[ORM\Column]
+    private ?int $coolFactor = null;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $plunderedAt;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    public function __construct()
+    {
+        $this->plunderedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function setTextDescription(string $description): static
+    {
+        $this->description = nl2br($description);
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCoolFactor(): ?int
+    {
+        return $this->coolFactor;
+    }
+
+    public function setCoolFactor(int $coolFactor): static
+    {
+        $this->coolFactor = $coolFactor;
+
+        return $this;
+    }
+
+    public function getPlunderedAt(): \DateTimeImmutable
+    {
+        return $this->plunderedAt;
+    }
+
+    /**
+     * A human-readable representation of when this treasure was plundered.
+     */
+    public function getPlunderedAtAgo(): string
+    {
+        return Carbon::instance($this->plunderedAt)->diffForHumans();
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setPublished(bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
